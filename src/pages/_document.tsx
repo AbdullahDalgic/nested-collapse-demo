537f776b0e7a076d741413a803ea022f3074f495
Abdullah Dalgıç <abdullahdalgic33@gmail.com>
2024-08-17 10:13:43 +0300

    feat: Add lazy loading for Google Analytics script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -7,6 +8,28 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+
+        <Script
+          defer
+          strategy="lazyOnload"
+          src="https://www.googletagmanager.com/gtag/js?id=G-53GM7QK75H"
+        />
+        <Script
+          id="google-analytics"
+          strategy="lazyOnload"
+          defer
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag() {
+                  dataLayer.push(arguments);
+                }
+                gtag("js", new Date());
+
+                gtag("config", "G-53GM7QK75H");
+              `,
+          }}
+        />
       </body>
     </Html>
   );
